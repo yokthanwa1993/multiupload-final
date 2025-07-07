b9d2c5e499437a8b7db352e2d46764f7c78d16d1
yokthanwa1993 <yokthanwa1993@gmail.com>
2025-07-07 19:25:17 +0700

    Add YouTube image domain to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'yt3.ggpht.com',
+      },
     ],
   },
   /* config options here */
